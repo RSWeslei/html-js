6300fa0b07db84dd9663bd8148e6b3529324dd84
RSWeslei <wrweslei@unochapeco.edu.br>
2022-06-22 18:27:14 -0300

    feat: adicionado opçao de exclusao de dados

--- index.js
+++ index.js
@@ -1,19 +1,68 @@
 const formulario = document.getElementById('formulario')
-const pessoaCadastradas = document.getElementById('pessoasCadastradas')
+const pessoasCadastradas = document.getElementById('pessoasCadastradas')
 const tableName = 'pessoas'
 
 popularTabelaAoRecarregar()
+adicionarEventoNosBotoesDeExclusao()
+preencherDados()
 
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault()
 
     let pessoa = arrayToObject($('#formulario').serializeArray())
-    adicionarPessoaNaTabela(pessoa)
+    
+    let pessoasCadastradas = JSON.parse(localStorage.getItem(tableName)) || []
 
-    let pessoas = JSON.parse(localStorage.getItem(tableName)) || []
-    pessoas.push(JSON.stringify(pessoa))
-    localStorage.setItem(tableName, JSON.stringify(pessoas))
+    let possuiCadastro = pessoasCadastradas
+        .map(pessoaCadastrada => (JSON.parse(pessoaCadastrada)).email) 
+        .includes(pessoa.email); 
+
+    if (possuiCadastro){
+        alert('Pessoa já cadastrada')
+        return
+    }
+
+    pessoasCadastradas.push(JSON.stringify(pessoa))
+    localStorage.setItem(tableName, JSON.stringify(pessoasCadastradas))
+    adicionarPessoaNaTabela(pessoa)
+    adicionarEventoNosBotoesDeExclusao()
+    preencherDados()
 })
+
+function adicionarEventoNosBotoesDeExclusao()
+{
+    $(".exclusao").toArray().forEach(botaoExclusao =>{
+        botaoExclusao.removeEventListener('click', (evento) => excluirPessoa(evento))
+        console.log('excluiu');
+    })
+
+    $(".exclusao").toArray().forEach(botaoExclusao =>{
+        botaoExclusao.addEventListener('click', (evento) => {
+            excluirPessoa(evento)
+        })
+    }) 
+}
+
+function excluirPessoa(evento){
+
+    let emailExcluir = evento.target.dataset.email
+    if (!confirm(`Deseja excluir o email ${emailExcluir}?`)){
+        return
+    }
+
+    let pessoasCadastradas = JSON.parse(localStorage.getItem(tableName)) || []
+    pessoasCadastradas = pessoasCadastradas.map(pessoa => JSON.parse(pessoa))
+
+    let indexExcluir = pessoasCadastradas.findIndex(pessoa => pessoa.email == emailExcluir)
+
+    console.log(indexExcluir)
+
+    pessoasCadastradas.splice(indexExcluir, 1)
+
+    pessoasCadastradas = pessoasCadastradas.map(pessoa => JSON.stringify(pessoa))
+    localStorage.setItem(tableName, JSON.stringify(pessoasCadastradas))
+    document.location.reload()
+}
 
 function popularTabelaAoRecarregar() {
     let pessoasLocalStorage = JSON.parse(localStorage.getItem(tableName)) || []
@@ -31,9 +80,12 @@ function adicionarPessoaNaTabela(pessoa) {
             <td>${pessoa.email}</td>
             <td>${pessoa.senha}</td>
             <td ${mudarCorRegiao(pessoa.regiao)}>${pessoa.regiao}</td>
+            <td><button class="btn btn-outline-danger exclusao" data-email="${pessoa.email}">
+                Excluir
+            </button></td>
         </tr>
     `
-    pessoaCadastradas.appendChild(tr)
+    pessoasCadastradas.appendChild(tr)
 }
 
 function mudarCorRegiao(regiao)
@@ -46,6 +98,24 @@ function mudarCorRegiao(regiao)
         "Centro-Oeste" : 'gray'
     }
     return `Style="color: ${corRegiao[regiao]}"` 
+}
+
+function preencherDados(){
+    let nome
+    let email
+    let senha
+    
+    nome = (Math.random() + 1).toString(36).substring(2)
+    email = nome + '@gmail.com'
+    senha = (Math.random() + 1).toString(36).substring(2)
+    regiao = 3
+    
+    console.log(document.getElementById('nome').value);
+    document.getElementById('nome').value = nome
+    document.getElementById('email').value = email
+    document.getElementById('senha').value = senha
+    document.getElementById('senhaConfirmar').value = senha
+    document.getElementById('regiao').value = 'Sul'
 }
 
 // o parametro "array" deve ser gerado a partir da funcao 
